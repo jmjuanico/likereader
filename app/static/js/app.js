82bef5acdfd6152cfd1dd3c12c8b6a2be736738d
jmjuanico <joey.juanico@gmail.com>
2016-01-24 16:19:47 +0800

    adding facebook and twitter logins

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -109,20 +109,22 @@ $(".nav li").on("click", function() {
 facebook sharing sdk
 ************************************************/
 
-window.fbAsyncInit = function() {
+  window.fbAsyncInit = function() {
     FB.init({
-    appId      : '1666096990295024',
-    xfbml      : true,
-    version    : 'v2.5'
+      appId      : '756338107834310',
+      xfbml      : true,
+      version    : 'v2.5'
     });
-};
+  };
 
-(function(d, s, id){
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {return;}
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+
 
 
